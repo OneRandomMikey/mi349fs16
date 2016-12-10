ad0f79dc3153fd9b98c49d0352a99dda6982f850
OneRandomMikey <zehua.li6@gmail.com>
2016-12-09 21:48:51 -0500

    Final Project

---
+++ Final_production/Final Project/js/index.js
@@ -0,0 +1,12 @@
+// $("button").on("click", function() {
+//   alert("Thank you!");
+// });
+
+
+function  {
+    document.getElementById("myCheck").click(); // Click on the checkbox
+}
+//
+// jQuery(document).ready(function(){
+//     $("header").text("Hello");
+// });
